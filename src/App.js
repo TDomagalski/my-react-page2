0ea6376a15091b155539d0ea55ac7be41f5d7d07
TDomagalski <tomekwebdev@gmail.com>
2022-10-11 13:55:01 +0200

    feat: add contact page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles/css/App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="about" element={<About />} />
+				<Route path="contact" element={<Contact />} />
 			</Routes>
 			<Footer />
 		</>
